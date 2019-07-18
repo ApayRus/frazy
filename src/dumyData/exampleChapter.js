bddcfc99de20aa201580910cab66ca37ab8dc0ca
aparus <rustamapaev@gmail.com>
2019-07-18 22:58:30 +0300

    dumy data added (source and complete json)

---
+++ src/dumyData/exampleChapter.js
@@ -0,0 +1,73 @@
+const audioLink = `https://booktits.firebaseapp.com/hobbit/audio/hobbit1_1.mp3`;
+
+const exampleChapter = {
+  audioLink,
+  phrases: {
+    '1': {
+      start: 0.04,
+      end: 1.11,
+      text: 'The Hobbit',
+      transl: { ru: 'Хоббит' }
+    },
+    '2': {
+      start: 4.35,
+      end: 5.63,
+      text: 'Chapter 1. ',
+      transl: { ru: 'Глава 1.' }
+    },
+    '3': {
+      start: 7.54,
+      end: 9.24,
+      text: 'An Unexpected Party ',
+      transl: {
+        ru:
+          'в норе под землей: «в земле»;  жил-был хоббит. #hole — дыра, отверстие'
+      }
+    },
+    '4': {
+      start: 13.08,
+      end: 15.74,
+      text: 'In a hole in the ground  there lived a hobbit. ',
+      transl: { ru: 'Хоббит' }
+    },
+    '5': {
+      start: 16.37,
+      end: 19.06,
+      text: 'Not a nasty, dirty, wet hole, ',
+      transl: { ru: 'не в отвратительной, грязной, мокрой норе, ' }
+    },
+    '6': {
+      start: 19.13,
+      end: 21.98,
+      text: 'filled with the ends of worms  and an oozy smell, ',
+      transl: {
+        ru:
+          'заполненной хвостами червей  и затхлым запахом тины # end — конец, край # oozy — илистый, влажный, '
+      }
+    },
+    '7': {
+      start: 21.98,
+      end: 27.29,
+      text:
+        'nor yet a dry, bare, sandy hole  with nothing in it to sit down on  or to eat: ',
+      transl: {
+        ru:
+          'но и не в сухой, голой, песчаной норе в которой нет ничего, на чем /можно/ посидеть или /что можно/ съесть: '
+      }
+    },
+    '8': {
+      start: 28.05,
+      end: 29.56,
+      text: 'it was a hobbit-hole, ',
+      transl: { ru: 'это была нора хоббита, ' }
+    },
+    '9': {
+      start: 29.56,
+      end: 31.73,
+      text: 'and that means comfort.',
+      transl: { ru: 'а это значит — комфортная /нора/.  ' }
+    }
+  }
+};
+
+export { exampleChapter };
---
+++ src/dumyData/textData.js
@@ -0,0 +1,340 @@
+const originalText = `
+The Hobbit
+Chapter 1. 
+An Unexpected Party 
+In a hole in the ground  there lived a hobbit. 
+Not a nasty, dirty, wet hole, 
+filled with the ends of worms  and an oozy smell, 
+nor yet a dry, bare, sandy hole  with nothing in it to sit down on  or to eat: 
+it was a hobbit-hole, 
+and that means comfort.
+It had a perfectly round door  like a porthole, 
+painted green, 
+with a shiny yellow brass knob  in the exact middle. 
+The door opened on  to a tube-shaped hall  like a tunnel: 
+a very comfortable tunnel  without smoke, 
+with panelled walls, and floors tiled and carpeted, 
+provided with polished chairs, 
+and lots and lots of pegs for hats and coats  — 
+the hobbit was fond of visitors.
+The tunnel wound on and on, 
+going fairly but not quite straight  into the side of the hill  — 
+The Hill, 
+as all the people for many miles round called it  — 
+and many little round doors  opened out of it, 
+first on one side  and then on another. 
+No going upstairs for the hobbit: 
+bedrooms, 
+bathrooms, cellars, 
+pantries  (lots of these), 
+wardrobes  
+(he had whole rooms devoted to clothes), 
+kitchens, 
+dining-rooms, 
+all were on the same floor, 
+and indeed on the same passage. 
+The best rooms were all on the left-hand side  (going in), 
+for these were the only ones  to have windows, 
+deep-set round windows  looking over his garden  and meadows beyond, 
+sloping down to the river.
+This hobbit was a very well-to-do hobbit, 
+and his name was Baggins. 
+The Bagginses had lived in the neighbourhood of The Hill  for time out of mind, 
+and people considered them very respectable, 
+not only because most of them were rich, 
+but also because they never had any adventures  
+or did anything unexpected: 
+you could tell what a Baggins would say  on any question  
+without the bother of asking him. 
+This is a story  of how a Baggins had an adventure, 
+and found himself  doing and saying things altogether unexpected. 
+He may have lost the neighbours’ respect, but he gained  — 
+well, you will see  whether he gained anything in the end.
+The mother of our particular hobbit  — 
+what is a hobbit? 
+I suppose hobbits need some description nowadays, 
+since they have become rare  and shy of the Big People, as they call us. 
+They are  
+(or were) 
+a little people, 
+about half our height, 
+and smaller than the bearded Dwarves. 
+Hobbits have no beards. 
+There is little or no magic about them, 
+except the ordinary everyday sort  which helps them to disappear quietly and quickly  
+when large stupid folk like you and me  come blundering along, 
+making a noise like elephants  which they can hear a mile off.
+They are inclined to be fat in the stomach; 
+they dress in bright colours  
+(chiefly green and yellow); 
+wear no shoes, 
+because their feet  grow natural leathery soles  
+and thick warm brown hair  like the stuff on their heads  
+(which is curly; 
+have long clever brown fingers, 
+good-natured faces, 
+and laugh deep fruity laughs  
+(especially after dinner, 
+which they have twice a day  when they can get it). 
+Now you know enough  to go on with.
+As I was saying, 
+the mother of this hobbit  — 
+of Bilbo Baggins, that is  — 
+was the fabulous 
+Belladonna Took, 
+one of the three remarkable daughters of the Old Took, 
+head of the hobbits  who lived across The Water, 
+the small river  that ran at the foot of The Hill. 
+It was often said  
+(in other families) 
+that long ago  one of the Took ancestors  
+must have taken a fairy wife. 
+That was, of course, 
+absurd, 
+but certainly there was still  something not entirely hobbit-like about them, — 
+and once in a while  
+members of the Took-clan would go and have adventures. 
+They discreetly disappeared, 
+and the family hushed it up; 
+but the fact remained  that the Tooks were not as respectable as the Bagginses, 
+though they were undoubtedly richer.
+Not that Belladonna Took ever had any adventures  after she became 
+Mrs. Bungo Baggins. 
+Bungo, 
+that was Bilbo’s father, 
+built the most luxurious hobbit-hole for her  
+(and partly with her money) 
+that was to be found  either under The Hill  
+or over The Hill  
+or across The Water, 
+and there they remained to the end of their days.     
+`;
+
+const translationText = `
+Хоббит
+Глава 1.
+Неожиданные гости 
+в норе под землей: «в земле»;  жил-был хоббит. #hole — дыра, отверстие
+не в отвратительной, грязной, мокрой норе, 
+заполненной хвостами червей  и затхлым запахом тины # end — конец, край # oozy — илистый, влажный, 
+но и не в сухой, голой, песчаной норе в которой нет ничего, на чем /можно/ посидеть или /что можно/ съесть: 
+это была нора хоббита, 
+а это значит — комфортная /нора/.  #comfort — утешение, успокоение; уют
+у нее была совершенно круглая дверь как иллюминатор, 
+выкрашенная в зеленый цвет, 
+с отполированной желтой медной круглой ручкой точно по середине. #shiny — ясный, солнечный, начищенный
+дверь открывалась /и вела/ в зал, в форме трубы похожий на туннель: # to open on — вести куда-либо /о двери/
+в очень благоустроенный туннель без копоти # smoke — дым, курение, 
+с обшитыми панелями стенами и полом, покрытым кафельной плиткой и ковром, 
+обставленный полированными стульями #to provide with — снабжать, обеспечивать, 
+и с множеством и множеством крючков для шляп и пальто  — 
+хоббит очень любил гостей.
+туннель продолжал виться и виться# on — зд. указывает на продолжение действия, 
+ведя почти что, но не совсем прямо в сторону холма  — 
+Холма, 
+как все люди на много миль вокруг называли его  — 
+и много маленьких круглых дверей открывались наружу /из него/, 
+сперва по одной стороне и затем по другой. 
+никаких хождений вверх /по лестнице/ для хоббита: 
+спальные комнаты, 
+ванные комнаты погреба, 
+кладовые для провизии множество кладовых: «таковых»), 
+гардеробные  
+у него были целые комнаты, предназначенные для одежды # to devote — посвящать, отдавать целиком, 
+кухни, 
+столовые, 
+все были расположены на одном и том же этаже, 
+и уж конечно, в одном и том же проходе. 
+все самые лучшие комнаты были с левой стороны со стороны входа), 
+так как это были единственные комнаты в которых были окна, 
+глубоко посаженные круглые окошки входящие на его сад и луга за ним, 
+спускающиеся к реке.
+этот самый хоббит был очень обеспеченным хоббитом, 
+и фамилия его была Бэггинс. 
+семья Бэггинсов жила в окрестностях Холма  с незапамятных времен # neighbourhood—соседство, близость # mind — ум, разум, 
+и люди считали их очень почтенными, 
+и не только потому, что большинство из них были богаты, 
+но также и потому, что они никогда не участвовали ни в каких приключениях  
+или /никогда не/ делали ничего неожиданного: 
+можно было сказать, что ответит один из Бэггинсов на любой вопрос  
+не утруждаясь даже спросить его # bother— беспокойство, хлопоты 
+эта история о том как Бэггинс попал в приключения, 
+и обнаружил внезапно, что он делает и говорит совершенно неожиданные вещи. # to find oneself doing smth. — сделать что-либо неожиданно для себя самого
+он, может быть, и потерял уважение соседей но он приобрел  — 
+ну, вы сами увидите приобрел ли он что-нибудь в конце концов.
+мать нашего этого хоббита - # particular — особый, специфический 
+а что такое хоббит? 
+я полагаю, что хоббитам необходимо /дать/ небольшое описание в наши дни, 
+так как они стали редкостью и стесняются Высокого Народа как они называют нас. # big — большой, крупный
+они /есть/  
+или были) 
+приземистым народом # little — маленький, небольшой, короткий, 
+где-то вполовину нашего роста, 
+и меньше, чем бородатые Гномы. 
+У хоббитов нет бороды. 
+в них мало, а то и совсем нет никакой магии, 
+за исключением обычной, повседневной разновидности /волшебства/ которое помогает им исчезать тихо и быстро  
+когда большие глупые люди, вроде тебя и меня идут ощупью # sort — вид, род, сорт  # blunder — грубая ошибка; промах; # to blunder along  — двигаться ощупью; спотыкаться, 
+и делают столько же шума, сколько слоны и который они могут слышать за целую милю.
+они имеют склонность быть толстенькими в брюшке; 
+они одеваются в одежду ярких цветов  
+большей частью зеленого и желтого); 
+не носят обувь, 
+потому что на их ступнях растут от природы кожистые подошвы  
+и густой теплый бурый мех: «каштановые волосы» как и мех: «волосы» на их головах # thick — толстый, жирный # stuff — материал, вещество   
+который /еще и/ кудрявый; 
+у них: «/они/ имеют» длинные ловкие смуглые пальцы # clever — умный, талантливый, умелый, 
+добродушные лица, 
+и смеются /они/ глубоким сочным смехом # fruity — фруктовый   
+особенно после обеда, 
+который у них два раза в день когда они могут его добыть). 
+теперь вы знаете достаточно чтобы продолжать.
+как я говорил, 
+мать этого самого хоббита  — 
+Бильбо Бэггинса, то есть  — 
+была легендарная 
+Белладонна Тук, 
+одна из трех замечательных дочерей Старого Тука, 
+главы хоббитов которые жили по ту сторону Воды, 
+маленькой речки которая текла у подножия Холма # to run / ran - бежать, гнать #feet — нога, ступня, основание 
+часто поговаривали  
+в других семьях) 
+что давным — давно один из предков семейства Туков  
+должно быть, взял в жены волшебницу. 
+это было, конечно же, 
+нелепо, 
+но, несомненно, было все-таки что— то не совсем хоббитовское: «похожее на хоббитов» в них, — 
+и, время от времени  
+члены клана Туков отправлялись и искали: «имели» приключения. 
+они незаметно: «обдуманно, осмотрительно» исчезали, 
+и /их/ семья заминала этот /факт/ # to hush — водворять тишину, успокаивать  
+но факт оставался фактом что семейство Туков не было таким же почтенным, как семейство Бэггинсов, 
+хотя они были, вне всякого сомнения, богаче.
+не то чтобы у Белладонны Тук случились какие-нибудь приключения
+после того, как она стала миссис Банго Бэггинс. 
+Банго, 
+который был отцом Бильбо, 
+построил самую что ни на есть роскошную хоббитовскую нору для нее  
+и частично на ее деньги) 
+которую можно было обнаружить либо под Холмом  
+или за Холмом  
+или по ту сторону Воды, 
+и там они оставались до конца своих дней. 
+`;
+const timingText = `
+Dialogue: 0,0:00:00.04,0:00:01.11,Default,,0,0,0,,
+Dialogue: 0,0:00:04.35,0:00:05.63,Default,,0,0,0,,
+Dialogue: 0,0:00:07.54,0:00:09.24,Default,,0,0,0,,
+Dialogue: 0,0:00:13.08,0:00:15.74,Default,,0,0,0,,
+Dialogue: 0,0:00:16.37,0:00:19.06,Default,,0,0,0,,
+Dialogue: 0,0:00:19.13,0:00:21.98,Default,,0,0,0,,
+Dialogue: 0,0:00:21.98,0:00:27.29,Default,,0,0,0,,
+Dialogue: 0,0:00:28.05,0:00:29.56,Default,,0,0,0,,
+Dialogue: 0,0:00:29.56,0:00:31.73,Default,,0,0,0,,
+Dialogue: 0,0:00:32.94,0:00:36.13,Default,,0,0,0,,
+Dialogue: 0,0:00:36.49,0:00:37.73,Default,,0,0,0,,
+Dialogue: 0,0:00:37.93,0:00:41.08,Default,,0,0,0,,
+Dialogue: 0,0:00:41.90,0:00:45.48,Default,,0,0,0,,
+Dialogue: 0,0:00:45.84,0:00:48.54,Default,,0,0,0,,
+Dialogue: 0,0:00:48.80,0:00:52.05,Default,,0,0,0,,
+Dialogue: 0,0:00:52.28,0:00:54.25,Default,,0,0,0,,
+Dialogue: 0,0:00:54.38,0:00:57.37,Default,,0,0,0,,
+Dialogue: 0,0:00:57.63,0:00:59.60,Default,,0,0,0,,
+Dialogue: 0,0:01:00.59,0:01:02.62,Default,,0,0,0,,
+Dialogue: 0,0:01:02.95,0:01:06.63,Default,,0,0,0,,
+Dialogue: 0,0:01:06.92,0:01:08.04,Default,,0,0,0,,
+Dialogue: 0,0:01:08.21,0:01:10.96,Default,,0,0,0,,
+Dialogue: 0,0:01:11.49,0:01:14.15,Default,,0,0,0,,
+Dialogue: 0,0:01:14.25,0:01:16.68,Default,,0,0,0,,
+Dialogue: 0,0:01:17.27,0:01:19.34,Default,,0,0,0,,
+Dialogue: 0,0:01:19.70,0:01:20.55,Default,,0,0,0,,
+Dialogue: 0,0:01:20.55,0:01:22.55,Default,,0,0,0,,
+Dialogue: 0,0:01:22.55,0:01:24.29,Default,,0,0,0,,
+Dialogue: 0,0:01:24.46,0:01:25.54,Default,,0,0,0,,
+Dialogue: 0,0:01:25.54,0:01:28.10,Default,,0,0,0,,
+Dialogue: 0,0:01:28.10,0:01:29.25,Default,,0,0,0,,
+Dialogue: 0,0:01:29.25,0:01:30.34,Default,,0,0,0,,
+Dialogue: 0,0:01:30.34,0:01:31.91,Default,,0,0,0,,
+Dialogue: 0,0:01:32.04,0:01:34.37,Default,,0,0,0,,
+Dialogue: 0,0:01:35.06,0:01:38.74,Default,,0,0,0,,
+Dialogue: 0,0:01:38.97,0:01:41.47,Default,,0,0,0,,
+Dialogue: 0,0:01:41.47,0:01:46.00,Default,,0,0,0,,
+Dialogue: 0,0:01:46.16,0:01:47.70,Default,,0,0,0,,
+Dialogue: 0,0:01:48.92,0:01:51.68,Default,,0,0,0,,
+Dialogue: 0,0:01:51.84,0:01:54.01,Default,,0,0,0,,
+Dialogue: 0,0:01:55.62,0:02:00.12,Default,,0,0,0,,
+Dialogue: 0,0:02:00.12,0:02:02.78,Default,,0,0,0,,
+Dialogue: 0,0:02:02.78,0:02:05.60,Default,,0,0,0,,
+Dialogue: 0,0:02:05.90,0:02:09.08,Default,,0,0,0,,
+Dialogue: 0,0:02:09.08,0:02:11.74,Default,,0,0,0,,
+Dialogue: 0,0:02:12.13,0:02:15.52,Default,,0,0,0,,
+Dialogue: 0,0:02:15.52,0:02:17.42,Default,,0,0,0,,
+Dialogue: 0,0:02:18.80,0:02:22.18,Default,,0,0,0,,
+Dialogue: 0,0:02:22.58,0:02:27.14,Default,,0,0,0,,
+Dialogue: 0,0:02:27.96,0:02:31.18,Default,,0,0,0,,
+Dialogue: 0,0:02:31.57,0:02:35.05,Default,,0,0,0,,
+Dialogue: 0,0:02:36.33,0:02:38.93,Default,,0,0,0,,
+Dialogue: 0,0:02:39.81,0:02:40.93,Default,,0,0,0,,
+Dialogue: 0,0:02:41.62,0:02:44.87,Default,,0,0,0,,
+Dialogue: 0,0:02:44.87,0:02:49.20,Default,,0,0,0,,
+Dialogue: 0,0:02:49.89,0:02:50.75,Default,,0,0,0,,
+Dialogue: 0,0:02:50.98,0:02:51.83,Default,,0,0,0,,
+Dialogue: 0,0:02:52.03,0:02:53.28,Default,,0,0,0,,
+Dialogue: 0,0:02:53.60,0:02:54.82,Default,,0,0,0,,
+Dialogue: 0,0:02:55.21,0:02:57.64,Default,,0,0,0,,
+Dialogue: 0,0:02:58.27,0:02:59.74,Default,,0,0,0,,
+Dialogue: 0,0:03:00.63,0:03:03.55,Default,,0,0,0,,
+Dialogue: 0,0:03:03.55,0:03:09.04,Default,,0,0,0,,
+Dialogue: 0,0:03:09.04,0:03:13.01,Default,,0,0,0,,
+Dialogue: 0,0:03:13.27,0:03:16.69,Default,,0,0,0,,
+Dialogue: 0,0:03:17.64,0:03:20.33,Default,,0,0,0,,
+Dialogue: 0,0:03:20.63,0:03:22.33,Default,,0,0,0,,
+Dialogue: 0,0:03:22.40,0:03:24.33,Default,,0,0,0,,
+Dialogue: 0,0:03:24.53,0:03:25.85,Default,,0,0,0,,
+Dialogue: 0,0:03:25.85,0:03:28.74,Default,,0,0,0,,
+Dialogue: 0,0:03:28.74,0:03:32.25,Default,,0,0,0,,
+Dialogue: 0,0:03:32.38,0:03:33.46,Default,,0,0,0,,
+Dialogue: 0,0:03:34.12,0:03:36.29,Default,,0,0,0,,
+Dialogue: 0,0:03:36.45,0:03:37.77,Default,,0,0,0,,
+Dialogue: 0,0:03:37.96,0:03:40.16,Default,,0,0,0,,
+Dialogue: 0,0:03:40.16,0:03:41.71,Default,,0,0,0,,
+Dialogue: 0,0:03:41.71,0:03:44.27,Default,,0,0,0,,
+Dialogue: 0,0:03:45.25,0:03:47.06,Default,,0,0,0,,
+Dialogue: 0,0:03:48.14,0:03:49.09,Default,,0,0,0,,
+Dialogue: 0,0:03:49.09,0:03:50.54,Default,,0,0,0,,
+Dialogue: 0,0:03:50.70,0:03:53.00,Default,,0,0,0,,
+Dialogue: 0,0:03:53.20,0:03:54.41,Default,,0,0,0,,
+Dialogue: 0,0:03:54.41,0:03:56.15,Default,,0,0,0,,
+Dialogue: 0,0:03:56.55,0:03:59.63,Default,,0,0,0,,
+Dialogue: 0,0:03:59.93,0:04:02.42,Default,,0,0,0,,
+Dialogue: 0,0:04:02.65,0:04:05.08,Default,,0,0,0,,
+Dialogue: 0,0:04:06.53,0:04:08.01,Default,,0,0,0,,
+Dialogue: 0,0:04:08.14,0:04:09.58,Default,,0,0,0,,
+Dialogue: 0,0:04:10.04,0:04:12.90,Default,,0,0,0,,
+Dialogue: 0,0:04:12.90,0:04:15.53,Default,,0,0,0,,
+Dialogue: 0,0:04:16.58,0:04:18.12,Default,,0,0,0,,
+Dialogue: 0,0:04:18.12,0:04:19.20,Default,,0,0,0,,
+Dialogue: 0,0:04:19.70,0:04:24.23,Default,,0,0,0,,
+Dialogue: 0,0:04:24.79,0:04:26.07,Default,,0,0,0,,
+Dialogue: 0,0:04:26.07,0:04:29.71,Default,,0,0,0,,
+Dialogue: 0,0:04:30.43,0:04:32.73,Default,,0,0,0,,
+Dialogue: 0,0:04:33.09,0:04:34.80,Default,,0,0,0,,
+Dialogue: 0,0:04:35.26,0:04:39.59,Default,,0,0,0,,
+Dialogue: 0,0:04:39.86,0:04:41.96,Default,,0,0,0,,
+Dialogue: 0,0:04:43.04,0:04:47.28,Default,,0,0,0,,
+Dialogue: 0,0:04:47.28,0:04:49.15,Default,,0,0,0,,
+Dialogue: 0,0:04:49.84,0:04:50.82,Default,,0,0,0,,
+Dialogue: 0,0:04:50.99,0:04:52.89,Default,,0,0,0,,
+Dialogue: 0,0:04:53.45,0:04:56.21,Default,,0,0,0,,
+Dialogue: 0,0:04:56.31,0:04:57.98,Default,,0,0,0,,
+Dialogue: 0,0:04:58.24,0:05:00.94,Default,,0,0,0,,
+Dialogue: 0,0:05:00.94,0:05:02.05,Default,,0,0,0,,
+Dialogue: 0,0:05:02.05,0:05:03.40,Default,,0,0,0,,
+Dialogue: 0,0:05:03.76,0:05:06.35,Default,,0,0,0,,
+`;
+
+const audioLink = `https://booktits.firebaseapp.com/hobbit/audio/hobbit1_1.mp3`;
+
+const textData = { originalText, translationText, timingText, audioLink };
+
+export { textData };
