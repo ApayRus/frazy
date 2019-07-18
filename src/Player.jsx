f93c5dca8e0b822c51b7225101ffa5f14b6b94cf
aparus <rustamapaev@gmail.com>
2019-07-18 22:58:56 +0300

    auto format settings

--- src/Player.jsx
+++ src/Player.jsx
@@ -3,12 +3,12 @@ import { Typography } from '@material-ui/core'
 import PlayerControls from './PlayerControls'
 
 function Player() {
-    return (
-        <div>
-            <Typography variant="h3">Player</Typography>
-            <PlayerControls />
-        </div>
-    )
+  return (
+    <div>
+      <Typography variant='h3'>Player</Typography>
+      <PlayerControls />
+    </div>
+  )
 }
 
 export default Player
