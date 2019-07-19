b81ce21d3f12b80fbba7ca211b931bc6c68af3ba
aparus <rustamapaev@gmail.com>
2019-07-19 16:17:23 +0300

    phrases component created

---
+++ src/Phrases.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Phrases.css'
+import { PlayArrow } from '@material-ui/icons'
+import { audio } from './howler'
+import { ButtonBase, Typography } from '@material-ui/core'
+
+function Phrases(props) {
+  const playPhrase = id => event => {
+    audio.pause()
+    audio.play(id)
+  }
+  const { phrases } = props
+
+  return (
+    <div className='phrases'>
+      {phrases.map(phrase => {
+        return (
+          <ButtonBase
+            style={{ display: 'block', width: '100%' }}
+            onClick={playPhrase(phrase.id)}
+            key={`phrase-${phrase.id}`}
+          >
+            <div className='phrase'>
+              <div className='id'>
+                {phrase.id} <PlayArrow fontSize='inherit' />{' '}
+              </div>
+              <div className='text'>
+                <Typography variant='body1'>{phrase.text}</Typography>
+              </div>
+              <div className='translation ru'>
+                <Typography variant='body2'>{phrase.transl.ru}</Typography>
+              </div>
+            </div>
+          </ButtonBase>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Phrases
--- src/Player.jsx
+++ src/Player.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Typography } from '@material-ui/core'
 import PlayerControls from './PlayerControls'
+import Phrases from './Phrases'
+import { exampleChapter } from './dumyData/exampleChapter'
+import { map } from 'lodash'
 
 function Player() {
+  let { phrases } = exampleChapter //object
+  phrases = map(phrases, (elem, key) => {
+    return { id: key, ...elem }
+  }) //array
+
   return (
     <div>
       <Typography variant='h3'>Player</Typography>
       <PlayerControls />
+      <Phrases phrases={phrases} />
     </div>
   )
 }
