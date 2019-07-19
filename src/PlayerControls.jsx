08005d7278a8cbca9e3aa306edae8c5e86adc30c
aparus <rustamapaev@gmail.com>
2019-07-19 09:27:58 +0300

    installed Howler.js , playing phrases with it

--- src/PlayerControls.jsx
+++ src/PlayerControls.jsx
@@ -1,30 +1,51 @@
 import React from 'react'
-import { PlayArrow, Pause, SkipNext, SkipPrevious, Replay, Edit } from '@material-ui/icons';
+import {
+  PlayArrow,
+  Pause,
+  SkipNext,
+  SkipPrevious,
+  Replay,
+  Edit
+} from '@material-ui/icons'
 import { IconButton } from '@material-ui/core'
+import { audio } from './howler'
 
 function PlayerControls() {
-    return (
-        <div className="playerControls">
-            <IconButton aria-label="Previous">
-                <SkipPrevious />
-            </IconButton>
-            <IconButton aria-label="Play">
-                <PlayArrow />
-            </IconButton>
-            <IconButton aria-label="Pause">
-                <Pause />
-            </IconButton>
-            <IconButton aria-label="Dictation">
-                <Edit />
-            </IconButton>
-            <IconButton aria-label="Replay">
-                <Replay />
-            </IconButton>
-            <IconButton aria-label="Next">
-                <SkipNext />
-            </IconButton>
-        </div>
-    )
+  const play = () => {
+    audio.play()
+  }
+
+  const pause = () => {
+    audio.pause()
+  }
+
+  const playPhrase = id => event => {
+    audio.pause()
+    audio.play(id)
+  }
+
+  return (
+    <div className='playerControls'>
+      <IconButton aria-label='Previous'>
+        <SkipPrevious />
+      </IconButton>
+      <IconButton onClick={play} aria-label='Play'>
+        <PlayArrow />
+      </IconButton>
+      <IconButton onClick={pause} aria-label='Pause'>
+        <Pause />
+      </IconButton>
+      <IconButton aria-label='Dictation'>
+        <Edit />
+      </IconButton>
+      <IconButton onClick={playPhrase('8')} aria-label='Replay'>
+        <Replay />
+      </IconButton>
+      <IconButton onClick={playPhrase('9')} aria-label='Next'>
+        <SkipNext />
+      </IconButton>
+    </div>
+  )
 }
 
 export default PlayerControls
