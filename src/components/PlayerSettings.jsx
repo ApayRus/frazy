dd2d03549c4f29d33656a955afa53c76bf64e7cb
aparus <rustamapaev@gmail.com>
2019-07-22 14:51:46 +0300

    created PlayerSettings UI elements all controls with event handlers

--- src/components/PlayerSettings.jsx
+++ src/components/PlayerSettings.jsx
@@ -1,20 +1,55 @@
 import React from 'react'
-import { Settings } from '@material-ui/icons'
-import { Typography } from '@material-ui/core'
+import { Settings as SettingsIcon } from '@material-ui/icons'
+import { Typography, Checkbox, Slider, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  grid: {
+    display: 'grid',
+    gridTemplateColumns: '2fr 1fr',
+    gridGap: '1px',
+    justifyItems: 'center',
+    alignItems: 'center'
+  }
+})
 
 function PlayerSettings() {
+  const classes = useStyles()
+
+  const handleChange = (event, newValue) => {
+    console.log('event.target.id, newValue', event.target.id, newValue)
+  }
+
   return (
-    <div>
+    <div style={{ margin: 10 }}>
       <Typography variant='h6'>
-        <Settings /> Player settings
+        <SettingsIcon style={{ marginTop: 5, fontSize: 17 }} /> Player settings
       </Typography>
-      <Typography variant='body1'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem expedita deserunt quia autem
-        quasi! Blanditiis necessitatibus eum dicta commodi provident laboriosam quod id repellendus
-        veritatis deserunt repudiandae iusto sapiente libero, omnis optio? Pariatur, dolores?
-        Accusamus beatae veniam natus. Blanditiis maiores explicabo adipisci sunt assumenda,
-        incidunt libero pariatur reprehenderit, repudiandae neque voluptatum nihil consectetur sequi
-      </Typography>
+
+      <Typography variant='subtitle1'>Display:</Typography>
+      <div className={classes.grid}>
+        <Typography variant='body2'>Show player</Typography>
+        <Checkbox id='showPlayer' onChange={handleChange} />
+        <Typography variant='body2'>Show original text</Typography>
+        <Checkbox id='showOriginalText' onChange={handleChange} />
+        <Typography variant='body2'>Show translation</Typography>
+        <Checkbox id='showTranslation' onChange={handleChange} />
+      </div>
+
+      <Typography variant='subtitle1'>Playback:</Typography>
+      <div className={classes.grid}>
+        <Typography variant='body2'>Volume</Typography>
+        <Slider id='volume' onChange={handleChange} style={{ width: '80%' }} />
+        <Typography variant='body2'>Playback rate</Typography>
+        <Slider id='playbackRate' onChange={handleChange} style={{ width: '80%' }} />
+      </div>
+
+      <Typography variant='subtitle1'>Dictation:</Typography>
+      <div className={classes.grid}>
+        <Typography variant='body2'>Repeat count</Typography>
+        <Slider id='repeatCount' onChange={handleChange} style={{ width: '80%' }} />
+        <Typography variant='body2'>Delay between repeats (x phrase length)</Typography>
+        <Slider id='delayBetweenRepeats' onChange={handleChange} style={{ width: '80%' }} />
+      </div>
     </div>
   )
 }
