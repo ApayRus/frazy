1c692d4a880b42adf61e934e2fa7c8dfddaccb85
aparus <rustamapaev@gmail.com>
2019-07-25 01:20:59 +0300

    added dummy data examples

---
+++ src/dumyData/units.js
@@ -0,0 +1,53 @@
+export const units = {
+  hobbit1_1: {
+    mediaLink: 'https://booktits.firebaseapp.com/hobbit/audio/hobbit1_1.mp3',
+    lang: 'en',
+    phrases: {
+      '1ode4easgv': {
+        start: 0.04,
+        end: 1.11,
+        text: 'The Hobbit'
+      },
+      q6ua1mrpcdg: {
+        start: 4.35,
+        end: 5.63,
+        text: 'Chapter 1. '
+      },
+      q100k7uc9oo: {
+        start: 7.54,
+        end: 9.24,
+        text: 'An Unexpected Party '
+      },
+      '5rvnsng7l2g': {
+        start: 13.08,
+        end: 15.74,
+        text: 'In a hole in the ground  there lived a hobbit. '
+      },
+      hah15q1q6fo: {
+        start: 16.37,
+        end: 19.06,
+        text: 'Not a nasty, dirty, wet hole, '
+      },
+      '806tvi51obg': {
+        start: 19.13,
+        end: 21.98,
+        text: 'filled with the ends of worms  and an oozy smell, '
+      },
+      lein5gglml: {
+        start: 21.98,
+        end: 27.29,
+        text: 'nor yet a dry, bare, sandy hole  with nothing in it to sit down on  or to eat: '
+      },
+      q1170tula4: {
+        start: 28.05,
+        end: 29.56,
+        text: 'it was a hobbit-hole, '
+      },
+      jsdhvifo85o: {
+        start: 29.56,
+        end: 31.73,
+        text: 'and that means comfort.'
+      }
+    }
+  }
+}
