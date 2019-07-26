b996c28a7b11ef2e625dbee28c4c6de2f05aa03c
aparus <rustamapaev@gmail.com>
2019-07-26 19:34:09 +0300

    renamed icons

--- src/components/PlayerControls.jsx
+++ src/components/PlayerControls.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import { PlayArrow, Pause, SkipNext, SkipPrevious, Replay, Edit } from '@material-ui/icons'
+import {
+  PlayArrow,
+  Pause,
+  SkipNext as PlayNext,
+  SkipPrevious as PlayPrev,
+  Replay,
+  Edit as Dictation
+} from '@material-ui/icons'
 import { IconButton } from '@material-ui/core'
 import { connect } from 'react-redux'
 //import { audio } from '../howler'
@@ -7,20 +14,10 @@ import { connect } from 'react-redux'
 function PlayerControls(props) {
   const { play, pause, playPhrase, isPlay, currentPhraseId } = props
 
-  /*
-  const pause = () => {
-    audio.pause()
-  }
-
-  const playPhrase = id => event => {
-    audio.pause()
-    audio.play(id)
-  } */
-
   return (
     <div className='playerControls'>
       <IconButton aria-label='Previous'>
-        <SkipPrevious />
+        <PlayPrev />
       </IconButton>
       {isPlay ? (
         <IconButton onClick={pause} aria-label='Pause'>
@@ -32,20 +29,23 @@ function PlayerControls(props) {
         </IconButton>
       )}
       <IconButton aria-label='Dictation'>
-        <Edit />
+        <Dictation />
       </IconButton>
       <IconButton onClick={playPhrase(currentPhraseId)} aria-label='Replay'>
         <Replay />
       </IconButton>
       <IconButton /* onClick={playPhrase('9')} */ aria-label='Next'>
-        <SkipNext />
+        <PlayNext />
       </IconButton>
     </div>
   )
 }
 
 const mapStateToProps = state => {
-  return { isPlay: state.playerState.play, currentPhraseId: state.playerState.currentPhraseId }
+  return {
+    isPlay: state.playerState.play,
+    currentPhraseId: state.playerState.currentPhraseId
+  }
 }
 
 export default connect(mapStateToProps)(PlayerControls)
