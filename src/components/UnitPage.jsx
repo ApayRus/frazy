ea5082595747c670b0e5923a1998104aaaae1a9a
aparus <rustamapaev@gmail.com>
2019-07-27 17:59:21 +0300

    now UnitPage diplays from redux via props

--- src/components/UnitPage.jsx
+++ src/components/UnitPage.jsx
@@ -2,28 +2,44 @@ import React, { useRef } from 'react'
 import PlayerControls from './PlayerControls'
 import Phrases from './Phrases'
 import Waveform from './Waveform'
+import Button from '@material-ui/core/Button'
 //REDUX
 import { connect } from 'react-redux'
-import { setUnitContent, setUnitTranslation } from '../store/pageContentActions'
 
-import { regions } from '../dumyData/regions'
+//TEMPORARY
+import { setUnitContent, setUnitTranslation, setPhrasesArray } from '../store/pageContentActions'
+
+// import { regions } from '../dumyData/regions'
+import { joinPhrasesAndTranslations } from '../utils/joinPhrasesAndTranslations'
 
 import { units } from '../dumyData/units'
 import { translations } from '../dumyData/translations'
 
 function UnitPage(props) {
   const waveformComponent = useRef(null)
-  const { unit, unitTranslations } = props
+  const {
+    unit,
+    unitTranslations,
+    phrasesArray,
+    setUnitContent,
+    setUnitTranslation,
+    setPhrasesArray
+  } = props
 
-  setUnitContent(units['hobbit1_1_en'])
-  setUnitTranslation(translations['hobbit1_1_ru'])
+  const setDataToRedux = () => {
+    setUnitContent(units['hobbit1_1_en'])
+    setUnitTranslation(translations['hobbit1_1_ru'])
+  }
 
-  console.log('unit', unit)
-  console.log('unitTranslations', unitTranslations)
+  const setPhrasesArrayToRedux = () => {
+    const phrasesArray = joinPhrasesAndTranslations(unit, unitTranslations['ru'])
+    setPhrasesArray(phrasesArray)
+  }
 
   const play = () => {
     waveformComponent.current.wavesurfer.play()
   }
+
   const pause = () => {
     waveformComponent.current.wavesurfer.pause()
   }
@@ -34,9 +50,19 @@ function UnitPage(props) {
 
   return (
     <div>
-      <Waveform ref={waveformComponent} />
-      <PlayerControls play={play} pause={pause} playPhrase={playPhrase} />
-      <Phrases phrases={regions} playPhrase={playPhrase} />
+      {phrasesArray.length ? (
+        <div>
+          <Waveform mediaLink={unit.mediaLink} regions={phrasesArray} ref={waveformComponent} />
+          <PlayerControls play={play} pause={pause} playPhrase={playPhrase} />
+          <Phrases phrases={phrasesArray} playPhrase={playPhrase} />
+        </div>
+      ) : (
+        <div style={{ marginTop: 200 }}>
+          <Button onClick={setDataToRedux}>1. Set Data to Redux</Button>
+          <br />
+          <Button onClick={setPhrasesArrayToRedux}>2. Set phrasesArray to Redux</Button>
+        </div>
+      )}
     </div>
   )
 }
@@ -46,14 +72,16 @@ function UnitPage(props) {
 const mapStateToProps = state => {
   return {
     unit: state.pageContent.unit,
-    unitTranslations: state.pageContent.unitTranslations
+    unitTranslations: state.pageContent.unitTranslations,
+    phrasesArray: state.pageContent.phrasesArray
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
     setUnitContent: payload => dispatch(setUnitContent(payload)),
-    setUnitTranslation: payload => dispatch(setUnitTranslation(payload))
+    setUnitTranslation: payload => dispatch(setUnitTranslation(payload)),
+    setPhrasesArray: payload => dispatch(setPhrasesArray(payload))
   }
 }
 
