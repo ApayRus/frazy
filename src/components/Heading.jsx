305fd316d9036902f8e10b84917ffa937efe4968
aparus <rustamapaev@gmail.com>
2019-07-28 22:45:54 +0300

    added Heading with navigation

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,74 +1,50 @@
 import React from 'react'
+import { heading } from '../content/GENERATEDheading.js'
+import { units } from '../content/GENERATEDunits.js'
+import { translations } from '../content/GENERATEDtranslations.js'
+import { List, ListItem, ListItemText } from '@material-ui/core'
+import { setPhrasesArray, setMediaLink } from '../store/pageContentActions'
+import { connect } from 'react-redux'
+import { joinPhrasesAndTranslations } from '../utils/joinPhrasesAndTranslations'
+import { toggleHeadingDrawer } from '../store/appStateActions'
+function Heading(props) {
+  const { setPhrasesArray, setMediaLink, toggleHeadingDrawer } = props
 
-function Heading() {
+  const handleClick = unitId => event => {
+    const unit = units[`hobbit${unitId}_en`]
+    const translation = translations[`hobbit${unitId}_ru`]
+    const phrasesArray = joinPhrasesAndTranslations(unit, translation)
+    const mediaLink = unit.mediaLink
+    toggleHeadingDrawer({ showHeadingDrawer: false })
+    setPhrasesArray(phrasesArray)
+    setMediaLink(mediaLink)
+  }
+
   return (
     <div>
       <h1>Heading!</h1>
-      <p>Lorem, ipsum dolor sit amet .</p>
-      <p>2.blabla</p>
-      <p>3.blabla</p>
-      <p>4.blabla</p>
-      <p>5.blabla</p>
-      <p>6.blabla</p>
-      <p>7.blabla</p>
-      <p>1.blabla</p>
-      <p>2.blabla</p>
-      <p>3.blabla</p>
-      <p>4.blabla</p>
-      <p>5.blabla</p>
-      <p>6.blabla</p>
-      <p>7.blabla</p>
-      <p>1.blabla</p>
-      <p>2.blabla</p>
-      <p>3.blabla</p>
-      <p>4.blabla</p>
-      <p>5.blabla</p>
-      <p>6.blabla</p>
-      <p>7.blabla</p>
-      <p>1.blabla</p>
-      <p>2.blabla</p>
-      <p>3.blabla</p>
-      <p>4.blabla</p>
-      <p>5.blabla</p>
-      <p>6.blabla</p>
-      <p>7.blabla</p>
-      <p>1.blabla</p>
-      <p>2.blabla</p>
-      <p>3.blabla</p>
-      <p>4.blabla</p>
-      <p>5.blabla</p>
-      <p>6.blabla</p>
-      <p>7.blabla</p>
-      <p>1.blabla</p>
-      <p>2.blabla</p>
-      <p>3.blabla</p>
-      <p>4.blabla</p>
-      <p>5.blabla</p>
-      <p>6.blabla</p>
-      <p>7.blabla</p>
-      <p>1.blabla</p>
-      <p>2.blabla</p>
-      <p>3.blabla</p>
-      <p>4.blabla</p>
-      <p>5.blabla</p>
-      <p>6.blabla</p>
-      <p>7.blabla</p>
-      <p>1.blabla</p>
-      <p>2.blabla</p>
-      <p>3.blabla</p>
-      <p>4.blabla</p>
-      <p>5.blabla</p>
-      <p>6.blabla</p>
-      <p>7.blabla</p>
-      <p>1.blabla</p>
-      <p>2.blabla</p>
-      <p>3.blabla</p>
-      <p>4.blabla</p>
-      <p>5.blabla</p>
-      <p>6.blabla</p>
-      <p>7.blabla</p>
+      <List>
+        {heading.map(elem => {
+          return (
+            <ListItem button onClick={handleClick(elem.id)} key={`heading-${elem.id}`}>
+              <ListItemText>{elem.title.ru}</ListItemText>
+            </ListItem>
+          )
+        })}
+      </List>
     </div>
   )
 }
 
-export default Heading
+const mapDispatchToProps = dispatch => {
+  return {
+    setPhrasesArray: payload => dispatch(setPhrasesArray(payload)),
+    setMediaLink: payload => dispatch(setMediaLink(payload)),
+    toggleHeadingDrawer: payload => dispatch(toggleHeadingDrawer(payload))
+  }
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Heading)
