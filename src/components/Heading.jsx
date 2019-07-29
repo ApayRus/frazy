3b72abbf24a77e298813b29c1fab79cf1ea82b99
aparus <rustamapaev@gmail.com>
2019-07-29 03:30:07 +0300

    book heading with styles

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
-import { heading } from '../content/GENERATEDheading.js'
+import { bookInfo } from '../content/GENERATEDheading.js'
 import { units } from '../content/GENERATEDunits.js'
 import { translations } from '../content/GENERATEDtranslations.js'
-import { List, ListItem, ListItemText } from '@material-ui/core'
+import { List, ListItem, ListItemText, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 import { setPhrasesArray, setMediaLink } from '../store/pageContentActions'
 import { connect } from 'react-redux'
 import { joinPhrasesAndTranslations } from '../utils/joinPhrasesAndTranslations'
 import { toggleHeadingDrawer } from '../store/appStateActions'
+
+const useStyles = makeStyles(theme => ({
+  header: {
+    textAlign: 'center',
+    padding: 20,
+    background: 'url("../img/menu-background.jpg")'
+  },
+  listItem: {
+    paddingTop: 0,
+    paddingBottom: 0,
+    fontSize: '12px',
+    lineHeight: 1
+  }
+}))
+
 function Heading(props) {
   const { setPhrasesArray, setMediaLink, toggleHeadingDrawer } = props
+  const classes = useStyles()
+  const heading = bookInfo.heading
+  const bookTitle = bookInfo.title.ru
+  const bookAuthor = bookInfo.author.ru
 
   const handleClick = unitId => event => {
     const unit = units[`hobbit${unitId}_en`]
@@ -25,11 +45,22 @@ function Heading(props) {
 
   return (
     <div>
-      <h1>Heading!</h1>
+      <div className={classes.header}>
+        <Typography variant='h6'>{bookTitle}</Typography>
+        <Typography variant='subtitle1'>{bookAuthor}</Typography>
+        <img alt={bookTitle} src='../img/logo.png' />
+      </div>
+
       <List>
         {heading.map(elem => {
           return (
-            <ListItem button onClick={handleClick(elem.id)} key={`heading-${elem.id}`}>
+            <ListItem
+              className={classes.listItem}
+              button
+              divider
+              onClick={handleClick(elem.id)}
+              key={`heading-${elem.id}`}
+            >
               <ListItemText>{elem.title.ru}</ListItemText>
             </ListItem>
           )
