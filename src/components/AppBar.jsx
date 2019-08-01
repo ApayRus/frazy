1469ef195aa6bd68eca887e996d6f7f4c05e3aa4
aparus <rustamapaev@gmail.com>
2019-08-02 01:33:48 +0300

    AppBar changed and moved to bottom

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,41 +1,57 @@
 import React from 'react'
-import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core'
-import { Menu, Settings, Help } from '@material-ui/icons'
+import { Fab } from '@material-ui/core'
+import { Menu as MenuIcon, Settings as SettingsIcon, Help as HelpIcon } from '@material-ui/icons'
+import { makeStyles } from '@material-ui/core/styles'
 import { connect } from 'react-redux'
 import { toggleHeadingDrawer, toggleSettingsDrawer } from '../store/appStateActions'
 
+const useStyles = makeStyles(theme => ({
+  bottom: {
+    position: 'fixed',
+    bottom: 2,
+    zIndex: 1
+  },
+  menu: {
+    left: 3
+  },
+  settings: {
+    right: 3
+  },
+  help: {
+    right: 55
+  }
+}))
+
 function Appbar(props) {
   const { toggleHeadingDrawer, toggleSettingsDrawer } = props
+  const classes = useStyles()
 
   return (
-    <AppBar>
-      <Toolbar>
-        <IconButton
-          onClick={() => {
-            toggleHeadingDrawer({ showHeadingDrawer: true })
-          }}
-          color='inherit'
-          aria-label='Menu'
-        >
-          <Menu />
-        </IconButton>
-        <Typography variant='h6'>FraZish</Typography>
-        <div style={{ marginLeft: 'auto' }}>
-          <IconButton color='inherit' aria-label='Settings'>
-            <Help />
-          </IconButton>
-          <IconButton
-            onClick={() => {
-              toggleSettingsDrawer({ showSettingsDrawer: true })
-            }}
-            color='inherit'
-            aria-label='Settings'
-          >
-            <Settings />
-          </IconButton>
-        </div>
-      </Toolbar>
-    </AppBar>
+    <div>
+      <Fab
+        className={`${classes.bottom} ${classes.menu}`}
+        onClick={() => {
+          toggleHeadingDrawer({ showHeadingDrawer: true })
+        }}
+        color='primary'
+        size='medium'
+      >
+        <MenuIcon />
+      </Fab>
+      <Fab className={`${classes.bottom} ${classes.help}`} color='primary' size='medium'>
+        <HelpIcon />
+      </Fab>
+      <Fab
+        className={`${classes.bottom} ${classes.settings}`}
+        onClick={() => {
+          toggleSettingsDrawer({ showSettingsDrawer: true })
+        }}
+        color='primary'
+        size='medium'
+      >
+        <SettingsIcon />
+      </Fab>
+    </div>
   )
 }
 
