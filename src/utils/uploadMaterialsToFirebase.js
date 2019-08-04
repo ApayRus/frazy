8f123124adc7ebb57d05272fd9281686bcf4f421
aparus <rustamapaev@gmail.com>
2019-08-04 19:26:56 +0300

    added comment  with usefull info about run firebase-admin

--- src/utils/uploadMaterialsToFirebase.js
+++ src/utils/uploadMaterialsToFirebase.js
@@ -5,6 +5,23 @@
 import admin from 'firebase-admin'
 import firebaseConfig from '../config/firebaseConfig.js'
 import serviceAccount from '../config/firebaseAdminKey.json'
+/**
+firebaseAdminKey.json is given by Firebase, on the project console: Project Overview --> Permissions --> Service accounts
+and  it should be replaced to .json. 
+{
+  "type": "***",
+  "project_id": "***",
+  "private_key_id": "****",
+  "private_key": "***",
+  "client_email": "****",
+  "client_id": "***",
+  "auth_uri": "***",
+  "token_uri": "***",
+  "auth_provider_x509_cert_url": "***",
+  "client_x509_cert_url": "***"
+}
+*/
+
 /* import materials from '../dumyData/GENERATEDmaterials.js'
 const collection = 'materials' */
 import materials from '../dumyData/GENERATEDtranslations.js'
