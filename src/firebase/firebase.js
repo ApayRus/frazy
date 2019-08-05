46cd660aa60a8a8f8307cf1aa75796a7eac2890f
aparus <rustamapaev@gmail.com>
2019-08-05 12:26:58 +0300

    added file storage

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,10 +1,12 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import 'firebase/storage'
 //npmimport 'firebase/auth'
 import firebaseConfig from '../config/firebaseConfig'
 
 firebase.initializeApp(firebaseConfig)
 
 firebase.firestore()
+firebase.storage()
 
 export default firebase
