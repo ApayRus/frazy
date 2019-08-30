2ee13ad96abc1b032c70527a47b9459c91ed4b6b
aparus <rustamapaev@gmail.com>
2019-08-31 02:33:26 +0300

    added MainPage , displays Events with links to materials

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
-import { Fab } from '@material-ui/core'
+import { Fab, List, ListItem, ListItemText } from '@material-ui/core'
 import { Add as AddIcon } from '@material-ui/icons'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { firestoreConnect, isLoaded } from 'react-redux-firebase'
+import * as moment from 'moment'
 
-function MainPage() {
+function MainPage(props) {
+  const { events } = props
+  let eventList = null
+
+  if (isLoaded(events)) {
+    eventList = (
+      <List>
+        {events.map(event => {
+          const { id, materialId, lang, trLang, title, trTitle, time, unit } = event
+          const primaryText = `${lang}: ${title}`
+          const secondaryText = `${trLang}: ${trTitle}, unit: ${unit}, time: ${moment(
+            time
+          ).fromNow()}`
+          return (
+            <ListItem
+              key={id}
+              component={Link}
+              to={`/material/${materialId}/${trLang}`}
+              button
+              divider
+            >
+              <ListItemText primary={primaryText} secondary={secondaryText} />
+            </ListItem>
+          )
+        })}
+      </List>
+    )
+  }
   const AddButton = () => {
     return (
       <Fab
@@ -20,9 +51,21 @@ function MainPage() {
 
   return (
     <div>
+      {eventList}
       <AddButton />
     </div>
   )
 }
 
-export default MainPage
+const mapStateToProps = state => {
+  console.log('state.firestore', state.firestore)
+  return { events: state.firestore.ordered.events }
+}
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect(props => {
+    //   const { materialId, trLang } = props.match.params
+    return [{ collection: 'events' }]
+  })
+)(MainPage)
