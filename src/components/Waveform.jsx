c385a1793317bb99d81ae2221e26334c05e8b3ea
aparus <rustamapaev@gmail.com>
2019-11-12 02:52:29 +0300

    rewritten component with useEffect

--- src/components/Waveform.jsx
+++ src/components/Waveform.jsx
@@ -1,62 +1,53 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import './Wavesurfer.css'
 import { CircularProgress, Typography } from '@material-ui/core'
 import wavesurferModule from '../wavesurfer/wavesurfer'
 
-export class Waveform extends Component {
-  constructor(props) {
-    super(props)
-    this.waveformElem = null
-    this.timelineElem = null
-    this.state = { isReady: false }
-  }
+function Waveform(props) {
+  let waveformElem,
+    timelineElem = null
+  const [isReady, setIsReady] = useState(false)
+  const { phrases, mediaLinkDownloadUrl, readOnly, waveformRenderProgress } = props
 
-  componentDidMount() {
-    const { phrases, mediaLinkDownloadUrl, readOnly } = this.props
-
-    // console.log('mediaLinkDownloadUrl3', mediaLinkDownloadUrl)
-
+  useEffect(() => {
+    //component will mount
     wavesurferModule.wavesurfer = wavesurferModule.init(
-      this.waveformElem,
-      this.timelineElem,
+      waveformElem,
+      timelineElem,
       mediaLinkDownloadUrl,
       phrases,
       readOnly
     )
 
     wavesurferModule.wavesurfer.on('ready', e => {
-      this.setState({ isReady: true })
+      setIsReady(true)
     })
-  }
 
-  componentWillUnmount() {
-    wavesurferModule.wavesurfer.destroy()
-    // console.log('Waveform unmounted')
-  }
+    return () => {
+      //component will UNmount
+      wavesurferModule.wavesurfer.destroy()
+    }
+  }, [mediaLinkDownloadUrl])
 
-  render() {
-    const { isReady } = this.state
-    const { waveformRenderProgress } = this.props
-    return (
-      <div className='waveform'>
-        {isReady ? (
-          ''
-        ) : (
-          <div style={{textAlign:"center"}}>
-            <Typography variant='body2' color='textSecondary'>
-              Waveform rendering...
-            </Typography>
-            <CircularProgress value={waveformRenderProgress} variant='static' color='secondary' />
-          </div>
-        )}
-        <div style={isReady ? {} : { visibility: 'hidden' }}>
-          <div ref={el => (this.waveformElem = el)} />
-          <div ref={el => (this.timelineElem = el)} />
+  return (
+    <div className='waveform'>
+      {isReady ? (
+        ''
+      ) : (
+        <div style={{ textAlign: 'center' }}>
+          <Typography variant='body2' color='textSecondary'>
+            Waveform rendering...
+          </Typography>
+          <CircularProgress value={waveformRenderProgress} variant='static' color='secondary' />
         </div>
+      )}
+      <div style={isReady ? {} : { visibility: 'hidden' }}>
+        <div ref={el => (waveformElem = el)} />
+        <div ref={el => (timelineElem = el)} />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
