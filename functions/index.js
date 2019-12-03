e8d3e6554a7b6dc34b0b376b190a9ac6ad636b5b
aparus <rustamapaev@gmail.com>
2019-12-03 04:28:16 +0300

    written cloud functions, firestore triggers that creates revisions  of Material and Translation

--- functions/index.js
+++ functions/index.js
@@ -11,8 +11,34 @@ admin.initializeApp(functions.config().firebase)
 //   response.send('Hello from Firebase -  Aparus!')
 // })
 
-// exports.materialCreated = functions.firestore.document('material/{materialId}').onUpdate(doc => {
-//   const docBefore = doc.before.data()
-//   //   const docAfter = doc.after.data()
-//   doc.after.ref.collection('revisions').add(docBefore)
-// })
+// revision - is an old (previous) version of document
+// after material or translation was updated, we add Revision with id = userId.
+// each user can have only one stored revision in doc
+const createRevision = collection =>
+  functions
+    .region('europe-west1')
+    .firestore.document(`${collection}/{doc}`)
+    .onUpdate(doc => {
+      const docBefore = doc.before.data()
+      //   const docAfter = doc.after.data()
+
+      const { userId, userName, time } = docBefore.meta.updated
+
+      //1) we write revision to subcollection
+      //revisionId is the UserId who created docBefore (last updated doc)
+      doc.after.ref
+        .collection('revisions')
+        .doc(userId)
+        .set(docBefore)
+        .catch(error => console.log(error))
+
+      //2) we write short info about revision to doc
+      const revisions = { [userId]: { userName, time } }
+      doc.after.ref.set({ meta: { revisions } }, { merge: true })
+    })
+
+//Firestore triggers:
+exports.materialUpdated = createRevision('material')
+exports.translationUpdated = createRevision('materialTr')
+
+//sudo firebase deploy --only functions
