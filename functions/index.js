ff870352498e767c3cbc77a1224af6050b42dfe2
aparus <rustamapaev@gmail.com>
2019-12-08 06:11:38 +0300

    write/rewrite Unit.heading when Material.title has changed

--- functions/index.js
+++ functions/index.js
@@ -3,19 +3,16 @@ const admin = require('firebase-admin')
 // const { detailedDiff } = require('deep-object-diff')
 
 admin.initializeApp(functions.config().firebase)
-// const db = admin.firestore()
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   response.send('Hello from Firebase -  Aparus!')
-// })
+const db = admin.firestore()
 
-// revision - is an old (previous) version of document
-// after material or translation was updated, we add Revision with id = userId.
-// each user can have only one stored revision in doc
-const createRevision = collection =>
-  functions
+/**
+ * revision - is an old (previous) version of document
+ * after material or translation was updated, we add Revision with id = userId.
+ * each user can have only one stored revision in doc subcollection 'Revisions'
+ * @param {string} collection
+ */
+function createRevision(collection) {
+  return functions
     .region('europe-west1')
     .firestore.document(`${collection}/{doc}`)
     .onUpdate(doc => {
@@ -36,9 +33,40 @@ const createRevision = collection =>
       const revisions = { [userId]: { userName, time } }
       doc.after.ref.set({ meta: { revisions } }, { merge: true })
     })
+}
+
+/**
+ * when doc in material has created/updated, if it has unit, and changed title,
+ * we write it to correspond doc in unit
+ *
+ * @param {string} collectionSourceOfAction
+ * @param {string} collectionRecieverOfAction
+ * @example
+ * rewriteHeading(material, unit) //
+ * rewriteHeading(materialTr, unitTr) //
+ */
+function rewriteHeading(collectionSourceOfAction, collectionRecieverOfAction) {
+  return functions
+    .region('europe-west1')
+    .firestore.document(`${collectionSourceOfAction}/{doc}`)
+    .onWrite((doc, context) => {
+      //   const docAfter = doc.after.data()
+      const { doc: docSourceId } = context.params
+      const { title: titleBefore = '' } = doc.before.data() || {}
+      const { unit: docRecieverId, title: titleAfter } = doc.after.data()
+
+      if (titleBefore !== titleAfter) {
+        db.collection(collectionRecieverOfAction)
+          .doc(docRecieverId)
+          .set({ heading: { [docSourceId]: { title: titleAfter } } }, { merge: true })
+          .catch(error => console.log(error))
+      }
+    })
+}
 
 //Firestore triggers:
 exports.materialUpdated = createRevision('material')
 exports.translationUpdated = createRevision('materialTr')
+exports.rewriteUnitHeading = rewriteHeading('material', 'unit')
 
 //sudo firebase deploy --only functions
