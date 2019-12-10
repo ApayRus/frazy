9ce9387014fe65ce981df185a48477fe3549354a
aparus <rustamapaev@gmail.com>
2019-12-10 07:11:55 +0300

    unit and order - fixed connection with redux

--- src/components/materialForm/MaterialFormInfo.jsx
+++ src/components/materialForm/MaterialFormInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField'
 import { makeStyles } from '@material-ui/core/styles'
-import { connect } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setPageParameter } from '../../store/pageContentActions'
 import MediaAddDeleteButton from './MediaAddDeleteButton'
 
@@ -13,49 +13,35 @@ const useStyles = makeStyles(theme => ({
 }))
 
 function MaterialInfo(props) {
+  const { unit, order } = useSelector(state => state.pageContent)
+  const dispatch = useDispatch()
   const classes = useStyles()
-  const { setPageParameter, unit, order } = props
 
   const handleChange = event => {
     const { id, value } = event.target
-    setPageParameter([id, value])
+    dispatch(setPageParameter([id, value]))
   }
 
   return (
     <div style={{ padding: 10 }}>
       <MediaAddDeleteButton />
       <TextField
-        defaultValue={unit}
+        value={unit}
         onChange={handleChange}
         className={classes.textField}
         id='unit'
         label='Unit'
       />
       <TextField
-        defaultValue={order}
+        value={order}
         onChange={handleChange}
         className={classes.textField}
         style={{ width: 60 }}
         id='order'
-        type='number'
         label='Order'
       />
     </div>
   )
 }
 
-const mapStateToProps = state => {
-  const pc = state.pageContent
-  return {
-    unit: pc.unit,
-    order: pc.order
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    setPageParameter: payload => dispatch(setPageParameter(payload))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(MaterialInfo)
+export default MaterialInfo
