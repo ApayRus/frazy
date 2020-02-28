629f5d5807d0e054c9ddf0862ad9c8601a8934c1
aparus <rustamapaev@gmail.com>
2020-02-28 15:57:33 +0300

    removed dummy data

--- src/dumyData/exampleHeading.js
+++
@@ -1,7 +0,0 @@
-const heading = [
-  { id: 'hobbit1_1_en', title: { ru: '1.1. Комфортабельная нора ' } },
-  { id: 'hobbit1_2_en', title: { ru: '1.2. Бильбо "второе издание" своего отца' } },
-  { id: 'hobbit1_3_en', title: { ru: '1.3. Гэндальф!' } }
-]
-
-export { heading }
