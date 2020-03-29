6888e6c1f282e1c0cb121cb301f75b7b30c9d086
aparus <rustamapaev@gmail.com>
2020-03-29 18:22:56 +0300

    HeaderDrawer now appears from left or right, related to language direction 'rtl' or 'ltr'

--- src/components/materialHeading/HeadingDrawerContainer.jsx
+++ src/components/materialHeading/HeadingDrawerContainer.jsx
@@ -3,15 +3,18 @@ import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
 import Heading from './Heading'
 import { toggleHeadingDrawer } from '../../store/appStateActions'
 import { useDispatch, useSelector } from 'react-redux'
+import { langDirection } from '../../theme/functions'
 
 function HeadingDrawerContainer() {
   const dispatch = useDispatch()
   const { showHeadingDrawer } = useSelector(state => state.appState)
+  const { lang } = useSelector(state => state.pageContent)
+  const direction = langDirection(lang)
 
   return (
     <div>
       <SwipeableDrawer
-        anchor='left'
+        anchor={direction === 'ltr' ? 'left' : 'right'}
         open={showHeadingDrawer}
         onOpen={() => dispatch(toggleHeadingDrawer({ showHeadingDrawer: true }))}
         onClose={() => dispatch(toggleHeadingDrawer({ showHeadingDrawer: false }))}
