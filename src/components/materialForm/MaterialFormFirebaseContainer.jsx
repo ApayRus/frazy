fa2e14f857a22f70b31334356702fe35464b4170
aparus <rustamapaev@gmail.com>
2020-04-02 05:04:48 +0300

    rewrittern with hooks and deleted clearCachedDocs (that caused infinite download on reload)

--- src/components/materialForm/MaterialFormFirebaseContainer.jsx
+++ src/components/materialForm/MaterialFormFirebaseContainer.jsx
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { firestoreConnect, isLoaded } from 'react-redux-firebase'
+import { useFirestoreConnect, isLoaded } from 'react-redux-firebase'
 import MaterialForm from './MaterialForm'
 import { fillPageContent } from '../../store/pageContentActions'
-import { clearCachedDocs } from '../../store/appStateActions'
-// import { setMenuParameter } from '../../store/menuActions'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
 /**
  * this component loads data from Firebase:  material and translation, join them and pass for display
- * @param {} props
  */
 function MaterialFormHOC(props) {
+  useFirestoreConnect(() => {
+    const { materialId, trLang } = props.match.params
+    return [
+      { collection: 'material', doc: materialId, storeAs: 'material' },
+      { collection: 'materialTr', doc: `${materialId}_${trLang}`, storeAs: 'translation' }
+    ]
+  })
   const dispatch = useDispatch()
   const { material, translation } = useSelector(state => state.firestore.data)
   const { materialId } = props.match.params
@@ -22,20 +26,10 @@ function MaterialFormHOC(props) {
       dispatch(fillPageContent({ materialId, material, translation, mode: 'forEdit' }))
       setAllDataIsLoaded(true)
     }
-    return () => {
-      //on unmount
-      dispatch(clearCachedDocs())
-    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [material, translation])
 
   return allDataIsLoaded ? <MaterialForm /> : <CircularProgress />
 }
 
-export default firestoreConnect(props => {
-  const { materialId, trLang } = props.match.params
-  return [
-    { collection: 'material', doc: materialId, storeAs: 'material' },
-    { collection: 'materialTr', doc: `${materialId}_${trLang}`, storeAs: 'translation' }
-  ]
-})(MaterialFormHOC)
+export default MaterialFormHOC
