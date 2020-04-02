0e3d3d8f8f982b6403b4e293641715f5df4b7655
aparus <rustamapaev@gmail.com>
2020-04-02 22:50:45 +0300

    added Facebook and Instagram links

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import IconButton from '@material-ui/core/IconButton'
-import { YouTube, Twitter, Mail, GitHub } from '@material-ui/icons'
+import { YouTube, Twitter, Mail, GitHub, Instagram, Facebook } from '@material-ui/icons'
 
 function SocialMedia(props) {
   const socialMedias = [
@@ -10,11 +10,21 @@ function SocialMedia(props) {
       icon: <YouTube />,
       href: 'https://www.youtube.com/channel/UCBVpYXb_SqRR-qc-oIprj-w'
     },
+    {
+      title: 'Facebook',
+      icon: <Facebook />,
+      href: 'https://www.facebook.com/Frazy-107277837599166/'
+    },
     { title: 'Mail', icon: <Mail />, href: 'mailto:admin@frazy.me' },
     {
       title: 'GitHub',
       icon: <GitHub style={{ fontSize: 20 }} />,
       href: 'https://github.com/Aparus/frazy'
+    },
+    {
+      title: 'Instagram',
+      icon: <Instagram />,
+      href: 'https://instagram.com/frazy_me'
     }
   ]
 
