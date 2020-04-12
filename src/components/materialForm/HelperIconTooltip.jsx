72e2346e4b4f482803f1813262d8b66864ea91a9
aparus <rustamapaev@gmail.com>
2020-04-12 03:12:10 +0300

    added open helper by click (not hover) also click away listener

--- src/components/materialForm/HelperIconTooltip.jsx
+++ src/components/materialForm/HelperIconTooltip.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import QuestionIcon from '@material-ui/icons/Help'
 import { Tooltip, IconButton } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import { blue } from '@material-ui/core/colors'
+import ClickAwayListener from '@material-ui/core/ClickAwayListener'
 
 const useStyles = makeStyles((theme) => ({
   tooltip: (props) => ({
@@ -10,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: 'none',
   }),
 
-  icon: (props) => ({
+  iconStyle: (props) => ({
     fontSize: props.iconSize,
     color: props.color,
   }),
@@ -24,14 +25,28 @@ const useStyles = makeStyles((theme) => ({
  * @param {number} props.fontSize - in px
  */
 function HelperIconTooltip(props) {
+  const [open, setOpen] = useState(false)
+  const toggleTooltip = () => setOpen(!open)
+  const closeTooltip = () => setOpen(false)
   const { color = blue[100], fontSize = 14, buttonSize = 'small', iconSize = 14 } = props
   const classes = useStyles({ fontSize, color, iconSize })
   return (
-    <Tooltip classes={classes} interactive title={props.title}>
-      <IconButton size={buttonSize}>
-        <QuestionIcon className={classes.icon} />
-      </IconButton>
-    </Tooltip>
+    <ClickAwayListener onClickAway={closeTooltip}>
+      <Tooltip
+        open={open}
+        onClose={toggleTooltip}
+        classes={classes}
+        interactive
+        title={props.title}
+        disableFocusListener
+        disableHoverListener
+        disableTouchListener
+      >
+        <IconButton onClick={toggleTooltip} size={buttonSize}>
+          <QuestionIcon className={classes.iconStyle} />
+        </IconButton>
+      </Tooltip>
+    </ClickAwayListener>
   )
 }
 
