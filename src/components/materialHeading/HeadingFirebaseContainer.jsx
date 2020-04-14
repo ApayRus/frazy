bf1e86e05a2036263b9b3c6ea09422e82c2f9587
aparus <rustamapaev@gmail.com>
2020-04-14 18:24:15 +0300

    rewritten with hooks

--- src/components/materialHeading/HeadingFirebaseContainer.jsx
+++ src/components/materialHeading/HeadingFirebaseContainer.jsx
@@ -1,70 +1,60 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { compose } from 'redux'
-import { firestoreConnect, isLoaded } from 'react-redux-firebase'
+import { useSelector, useDispatch } from 'react-redux'
+import { useFirestoreConnect, isLoaded } from 'react-redux-firebase'
 import { setMenuParameter } from '../../store/menuActions'
 import CircularProgress from '@material-ui/core/CircularProgress'
-import HeadingDrawerContainer from './HeadingDrawerContainer'
+import HeadingDrawer from './HeadingDrawerContainer'
+import HeadingPage from './HeadingPage'
 import { afterFirebaseFileDownloadUrlReady } from '../../utils/firebase'
 
 /**
  * this component loads data from Firebase
  * @param {} props
  */
-function HeadingFirebaseHOC(props) {
-  const { setMenuParameter, unit } = props
+function HeadingFirebaseContainer(props) {
+  const { unit } = useSelector((state) => state.firestore.data)
+  const { unitId, displayMode } = props
+  const dispatch = useDispatch()
+
+  useFirestoreConnect(() => {
+    return [{ collection: 'unit', doc: unitId, storeAs: 'unit' }]
+  })
 
   const setParamAsync = (paramName, firebaseId) =>
-    afterFirebaseFileDownloadUrlReady(firebaseId, url => setMenuParameter([paramName, url]))
+    afterFirebaseFileDownloadUrlReady(firebaseId, (url) =>
+      dispatch(setMenuParameter([paramName, url]))
+    )
 
   const defaultUnitInfo = {
     author: 'There is no info for this unit yet',
     title: 'There should be a title here',
     logo: 'default-files/default-logo.png',
-    background: 'default-files/default-background.jpg'
+    background: 'default-files/default-background.jpg',
   }
 
   if (isLoaded(unit)) {
     if (unit) {
       //may be unitInfo not exists
       const { title, author, logo, background } = unit
-      setMenuParameter(['title', title ? title : defaultUnitInfo.title])
-      setMenuParameter(['author', author ? author : defaultUnitInfo.author])
+      dispatch(setMenuParameter(['title', title ? title : defaultUnitInfo.title]))
+      dispatch(setMenuParameter(['author', author ? author : defaultUnitInfo.author]))
       setParamAsync('logo', logo ? logo : defaultUnitInfo.logo)
       setParamAsync('background', background ? background : defaultUnitInfo.background)
       if (unit.heading) {
-        setMenuParameter(['heading', unit.heading])
+        dispatch(setMenuParameter(['heading', unit.heading]))
       }
     } else {
       //default heading when unit data not exists
-      setMenuParameter(['title', defaultUnitInfo.title])
-      setMenuParameter(['author', defaultUnitInfo.author])
+      dispatch(setMenuParameter(['title', defaultUnitInfo.title]))
+      dispatch(setMenuParameter(['author', defaultUnitInfo.author]))
       setParamAsync('logo', defaultUnitInfo.logo)
       setParamAsync('background', defaultUnitInfo.background)
     }
 
-    return <HeadingDrawerContainer />
+    return displayMode === 'drawer' ? <HeadingDrawer /> : <HeadingPage />
   } else {
     return <CircularProgress />
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    unit: state.firestore.data.unit
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    setMenuParameter: payload => dispatch(setMenuParameter(payload))
-  }
-}
-
-export default compose(
-  connect(mapStateToProps, mapDispatchToProps),
-  firestoreConnect(props => {
-    const { unitId } = props
-    return [{ collection: 'unit', doc: unitId, storeAs: 'unit' }]
-  })
-)(HeadingFirebaseHOC)
+export default HeadingFirebaseContainer
