a7f389218ed4711bc90d83fe71c9d9d899bc0c92
aparus <rustamapaev@gmail.com>
2020-06-07 23:44:26 +0300

    unit form grid settings and styling

--- src/components/unit/UnitForm.jsx
+++ src/components/unit/UnitForm.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField'
 import { useSelector } from 'react-redux'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  field: {
+    width: '100%',
+    marginTop: 5,
+    marginBottom: 5
+  }
+}))
 
 function UnitForm() {
+  const classes = useStyles()
   const { lang, title, author, description, logo, background } = useSelector(state => state.menu)
   return (
-    <div style={{ width: '100%' }}>
-      <TextField value={lang} label='Lang' />
-      <TextField value={title} label='Title' />
-      <TextField value={author} label='Author' />
-      <TextField value={description} multiline label='Description' />
-      <TextField value={logo} label='Logo' />
-      <TextField value={background} label='Background' />
+    <div>
+      <TextField className={classes.field} value={lang} label='Lang' />
+      <TextField className={classes.field} value={title} label='Title' />
+      <TextField className={classes.field} value={author} label='Author' />
+      <TextField className={classes.field} value={description} multiline label='Description' />
+      <TextField className={classes.field} value={logo} label='Logo' />
+      <TextField className={classes.field} value={background} label='Background' />
     </div>
   )
 }
--- src/components/unit/UnitPage.jsx
+++ src/components/unit/UnitPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import HeadingFirebaseContainer from './UnitPageDataContainer'
 import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 import { MuiThemeProvider, makeStyles } from '@material-ui/core/styles'
 import { langTheme, LangFonts } from '../../theme/functions'
 import { useSelector, useDispatch } from 'react-redux'
@@ -9,13 +11,29 @@ import ButtonWithAuthPopover from '../auth/ButtonWithAuthPopover'
 import { loginDialog as local } from '../../localization/en'
 import { setAppStateParam } from '../../store/appStateActions'
 import UnitForm from './UnitForm'
+import clsx from 'clsx'
 
 const useStyles = makeStyles(theme => ({
   background: props => ({
     backgroundImage: `linear-gradient(rgba(255,255,255,0.9), rgba(255,255,255,0.7)), url(${props.background})`
   }),
   editForm: {
-    padding: 10
+    marginBottom: 10
+  },
+  blockTitle: {
+    backgroundColor: 'skyblue',
+    width: '100%',
+    textAlign: 'center'
+  },
+  middleBlock: {
+    [theme.breakpoints.up('md')]: {
+      paddingLeft: 16,
+      paddingRight: 16
+    },
+    [theme.breakpoints.only('sm')]: {
+      paddingLeft: 8,
+      paddingRight: 8
+    }
   }
 }))
 
@@ -27,10 +45,12 @@ function HeadingPage(props) {
   const classes = useStyles({ background })
   const dispatch = useDispatch()
 
+  const toggleEditMode = () => dispatch(setAppStateParam({ editMode: !editMode }))
+
   const EditButton = () => (
     <div style={{ position: 'fixed', bottom: 2, zIndex: 1, right: 2 }}>
       <ButtonWithAuthPopover
-        actionOnSuccess={() => dispatch(setAppStateParam({ editMode: !editMode }))}
+        actionOnSuccess={toggleEditMode}
         message={local.loginButtonMessageForAddMaterial}
         buttonIcon={<EditIcon />}
       />
@@ -39,7 +59,7 @@ function HeadingPage(props) {
 
   return (
     <MuiThemeProvider theme={theme}>
-      <div style={!editMode ? { display: 'block' } : { display: 'none' }}>
+      <div style={{ display: !editMode ? 'block' : 'none' }}>
         <Grid container>
           <Grid className={classes.background} item xs={0} sm={2} md={4}></Grid>
           <Grid item xs={12} sm={8} md={4}>
@@ -52,19 +72,40 @@ function HeadingPage(props) {
         </Grid>
       </div>
 
-      <div style={editMode ? { display: 'block' } : { display: 'none' }}>
+      <div style={{ display: editMode ? 'block' : 'none' }}>
         <Grid container>
           <Grid item xs={12} sm={4} md={4}>
+            <div className={classes.blockTitle}>
+              <Typography variant='subtitle1'>Preview</Typography>
+            </div>
             <div style={{ boxShadow: `0px 1px 5px lightgrey` }}>
               <HeadingFirebaseContainer unitId={unitId} displayMode='page' />
-              <EditButton />
             </div>
           </Grid>
-          <Grid className={classes.editForm} item xs={12} sm={4} md={4}>
+          <Grid className={clsx(classes.editForm, classes.middleBlock)} item xs={12} sm={4} md={4}>
+            <div className={classes.blockTitle}>
+              <Typography variant='subtitle1'>Unit (original)</Typography>
+            </div>
             <UnitForm />
           </Grid>
           <Grid className={classes.editForm} item xs={12} sm={4} md={4}>
+            <div className={classes.blockTitle}>
+              <Typography variant='subtitle1'>Unit (translation)</Typography>
+            </div>
             <UnitForm />
+            <div style={{ marginTop: 20, padding: 5, textAlign: 'right' }}>
+              <Button
+                variant='contained'
+                color='default'
+                style={{ marginRight: 10 }}
+                onClick={toggleEditMode}
+              >
+                Cancel
+              </Button>
+              <Button variant='contained' color='primary'>
+                Save
+              </Button>
+            </div>
           </Grid>
         </Grid>
       </div>
