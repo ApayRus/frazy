0419333856742f73cba930965d83064ff2fdb268
aparus <rustamapaev@gmail.com>
2020-06-07 10:21:54 +0300

    switching between edit and view modes

--- src/components/unit/UnitPage.jsx
+++ src/components/unit/UnitPage.jsx
@@ -3,44 +3,72 @@ import HeadingFirebaseContainer from './UnitPageDataContainer'
 import Grid from '@material-ui/core/Grid'
 import { MuiThemeProvider, makeStyles } from '@material-ui/core/styles'
 import { langTheme, LangFonts } from '../../theme/functions'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import EditIcon from '@material-ui/icons/Edit'
 import ButtonWithAuthPopover from '../auth/ButtonWithAuthPopover'
 import { loginDialog as local } from '../../localization/en'
+import { setAppStateParam } from '../../store/appStateActions'
+import UnitForm from './UnitForm'
 
 const useStyles = makeStyles(theme => ({
   background: props => ({
     backgroundImage: `linear-gradient(rgba(255,255,255,0.9), rgba(255,255,255,0.7)), url(${props.background})`
-  })
+  }),
+  editForm: {
+    padding: 10
+  }
 }))
 
 function HeadingPage(props) {
-  const AddButton = () => (
+  const { unitId = '' } = props.match.params || {}
+  const { lang, background } = useSelector(state => state.menu)
+  const { editMode } = useSelector(state => state.appState)
+  const theme = langTheme(lang)
+  const classes = useStyles({ background })
+  const dispatch = useDispatch()
+
+  const EditButton = () => (
     <div style={{ position: 'fixed', bottom: 2, zIndex: 1, right: 2 }}>
       <ButtonWithAuthPopover
-        actionOnSuccess={() => console.log('switch edit mode')}
+        actionOnSuccess={() => dispatch(setAppStateParam({ editMode: !editMode }))}
         message={local.loginButtonMessageForAddMaterial}
         buttonIcon={<EditIcon />}
       />
     </div>
   )
 
-  const { unitId = '' } = props.match.params || {}
-  const { lang, background } = useSelector(state => state.menu)
-  const theme = langTheme(lang)
-  const classes = useStyles({ background })
   return (
     <MuiThemeProvider theme={theme}>
-      <Grid container>
-        <Grid className={classes.background} item xs={0} sm={2} md={4}></Grid>
-        <Grid item xs={12} sm={8} md={4}>
-          <div style={{ boxShadow: `0px 1px 5px lightgrey` }}>
-            <HeadingFirebaseContainer unitId={unitId} displayMode='page' />
-            <AddButton />
-          </div>
+      <div style={!editMode ? { display: 'block' } : { display: 'none' }}>
+        <Grid container>
+          <Grid className={classes.background} item xs={0} sm={2} md={4}></Grid>
+          <Grid item xs={12} sm={8} md={4}>
+            <div style={{ boxShadow: `0px 1px 5px lightgrey` }}>
+              <HeadingFirebaseContainer unitId={unitId} displayMode='page' />
+              <EditButton />
+            </div>
+          </Grid>
+          <Grid className={classes.background} item xs={0} sm={2} md={4}></Grid>
         </Grid>
-        <Grid className={classes.background} item xs={0} sm={2} md={4}></Grid>
-      </Grid>
+      </div>
+
+      <div style={editMode ? { display: 'block' } : { display: 'none' }}>
+        <Grid container>
+          <Grid item xs={12} sm={4} md={4}>
+            <div style={{ boxShadow: `0px 1px 5px lightgrey` }}>
+              <HeadingFirebaseContainer unitId={unitId} displayMode='page' />
+              <EditButton />
+            </div>
+          </Grid>
+          <Grid className={classes.editForm} item xs={12} sm={4} md={4}>
+            <UnitForm />
+          </Grid>
+          <Grid className={classes.editForm} item xs={12} sm={4} md={4}>
+            <UnitForm />
+          </Grid>
+        </Grid>
+      </div>
+
       <LangFonts lang={lang} />
     </MuiThemeProvider>
   )
--- src/store/appStateReducer.js
+++ src/store/appStateReducer.js
@@ -1,18 +1,20 @@
 const initState = {
-  showHeadingDrawer: false,
-  showSettingsDrawer: false,
-  showFullscreenDialog: false,
-  langsUserInterestedIn: [],
+    showHeadingDrawer: false,
+    showSettingsDrawer: false,
+    showFullscreenDialog: false,
+    langsUserInterestedIn: [],
+    editMode: false
 }
 
 const appStateReducer = (state = initState, action) => {
-  switch (action.type) {
-    case 'SET_APP_STATE_PARAM': {
-      return { ...state, ...action.payload }
+    switch (action.type) {
+        case 'SET_APP_STATE_PARAM':
+            {
+                return {...state, ...action.payload }
+            }
+        default:
+            return state
     }
-    default:
-      return state
-  }
 }
 
 export default appStateReducer
