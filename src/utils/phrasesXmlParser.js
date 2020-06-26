e6affd04b6bb19ba3272c612688841f03e514178
aparus <rustamapaev@gmail.com>
2020-06-27 01:13:45 +0300

    xml-parser refactored, dict tag removed

--- src/utils/phrasesXmlParser.js
+++ src/utils/phrasesXmlParser.js
@@ -1,147 +1,103 @@
+function fullTagToObject(inputTag) {
+    // clear tag text
+    let inputText = inputTag.replace(/<(.+)\/*\s*>/, "$1");
+    // extract tag and remove it from text
+    let [, tag = ""] = inputText.match(/\s*(.+?)\s+/) || [];
+    tag = tag.toLowerCase();
+    const allowedTags = ["img", "actor", "comment"];
+    if (!allowedTags.includes(tag)) return null;
+    inputText = inputText.replace(/^\s*.+?\s+/, ""); // removed tag - first word in text
+
+    if (!inputText) return null; // <actor     /> - tag exists but text - not
+
+    const attsArray = inputText.match(/\s*.+?=\s*?".+?"/g);
+
+    const attributes = attsArray ?
+        attsArray.reduce((obj, elem) => {
+            const attributeArray = elem.trim().split("=");
+            const [key, value] = attributeArray;
+            obj[key.trim()] = value.replace(/['"]/g, "").trim();
+            return obj;
+        }, {}) : {
+            [getDefaultAttribute(tag)]: inputText
+        }; // <actor Neil /> , <img http://domain.com/file.png />
+
+    return {
+        [tag]: {...attributes }
+    };
+}
+
 /**
- * parsing of xml like  phrase string into object with params:
- *
- * actor
- * text
- * dict
- * comment
- *
- * <actor name="Neil Armostrong" /> Hello, I'm Neil.
- * <dict #1 word1Form1, word1Form2 - translations of word; #2 word2Form1, word2Form2 - translations of word; />
- * <comment Эта фраза встречается только в редких случаях />
- * <actor name="Neil Armostrong" /> Hello, I'm Neil. <dict #0 bla - bla />
- *
- * also available short tags.
- * <Neil>, <Neil /> will be interpreted like <actor name="Nail">
- * <#1 word1 - translation; #2 word2 - translation2;> (with / or without /) - will be interpretted like  <dict>
- *
- * Register of tag always will be changed to lower case. You can youse <ACTOR> and <actor>, both will be the same
- *
- * @param {string} phraseLine - string with xml-like parts : <actor /> <dict /> <comment /> or just plain text
- * @example
- * const phraseLine = '<Neil> Hello! This is 6 minute English. <#1 hello - greeting; #2 English - language of Britain >'
- * phraseTextToObject(phraseLine) // will return :
- * {
- * actor: {name: 'Neil'},
- * text: 'Hello! This is 6 minute English.',
- * dict: [
- *   { wordOrder: 1, wordForms: "hello", wordTranslations: 'greeting; ' },
- *   { wordOrder: 2, wordForms: "English", wordTranslations: 'language of Britain' },
- *  ]
- * }
+<http://domain.com/file-path.jpg> => <img src="http://domain.com/file-path.jpg" /> 
+<Nail> - at the beginning => <actor name="Nail" /> 
+<Nail> - at the end => <comment text="Nail" /> 
  */
+function shortTagToObject(shortXml, phraseLine) {
+    const addTagToShortXml = (shortXml, tag) => {
+        return tag ? shortXml.replace(/^\s*</, `<${tag} `) : null;
+    };
+    let fullTag = "";
+    if (shortXml.match(/https*/)) {
+        fullTag = addTagToShortXml(shortXml, "img");
+    } else {
+        const positionOfMatch = getPositionOfSubstringInString(
+            shortXml,
+            phraseLine
+        );
+        const tag = getDefaultTag(positionOfMatch);
+        fullTag = addTagToShortXml(shortXml, tag);
+    }
+    return fullTag ? fullTagToObject(fullTag) : null;
+}
+
 export function phraseTextToObject(phraseLine) {
-  const xmlPattern = /<.+?\/*>/g
-  // 1) we find all xml-like patterns in the string
-  const match = phraseLine.match(xmlPattern) || []
-  // 2) if xml tag is with props like name="", src="", we do:
-  const phraseObject = match.reduce((obj, elem) => {
-    const tagObject = xmlTagToObject(elem)
-    return { ...obj, ...tagObject }
-  }, {})
-  // after we have extracted all xml-tags, we remove them and save the rest like "text"
-  phraseObject['text'] = phraseLine.replace(xmlPattern, '')
+    const xmlPattern = /<.+?\/*>/g;
+    // 1) we find all xml-like patterns in the string
 
-  return phraseObject
+    const match = phraseLine.match(xmlPattern) || [];
+    // console.log("match", match);
+
+    // 2) if xml tag is with props like name="", src="", we do:
+    const phraseObject = match.reduce((obj, elem) => {
+        // console.log(phraseLine.find(elem))
+        const tagObject = tagToObject(elem, phraseLine);
+        return {...obj, ...tagObject };
+    }, {});
+    // after we have extracted all xml-tags, we remove them and save the rest like "text"
+    phraseObject["text"] = phraseLine.replace(xmlPattern, "");
+
+    return phraseObject;
 }
 
-/**
-   * if tag contains attributes (key="value"), will be return object with all this attributes
-   * else, if tag doesn't contain attributes, will return {[tagName]: { text }}
-   * in case of tag [dict] will be return more complicated object
-   * @example
-   * const actorText = `<ACTOR name="Neil Armostrong" src="https://randomuser.me/api/portraits/men/32.jpg" />`;
-   * xmlTagToObject(actorText) // will return
-   * { actor: {
-   *      name: "Neil Armostrong",
-   *      src: "https://randomuser.me/api/portraits/men/32.jpg"
-   *    } }
-   * const dictText = `<DICT #1 word1, word2 - translation of first word; #2 word3, word4 - translation of second word; />`
-   *  xmlTagToObject(dictText) // will return
-  {"dict": {
-    "0": {
-     "wordOrder": 1,
-     "wordForms": "word1, word2",
-     "wordTranslations": "translation of first word; "
-    },
-    "1": {
-     "wordOrder": 2,
-     "wordForms": "word3, word4",
-     "wordTranslations": "translation of second word; "
-    }
-   }}
-   */
-function xmlTagToObject(xmlString) {
-  const existingTags = ['actor', 'dict', 'comment']
-  let tag = xmlString
-    .split(' ')[0]
-    .replace(/^\s*</, '')
-    .toLowerCase()
-
-  const attributesText = xmlString.match(/\s+.+?=\s*".+?"/g) //key="value"
-  if (attributesText) {
-    const attributes = attributesText.reduce((obj, elem) => {
-      const attributeArray = elem.trim().split('=')
-      const [key, value] = attributeArray
-      obj[key] = value.replace(/['"]/g, '')
-      return obj
-    }, {})
-    return { [tag]: { ...attributes } }
-  } else {
-    let text = xmlString.split(' ')
-    if (existingTags.includes(tag)) {
-      text.shift() //1-st word in xmlString was tag name
-    } else {
-      // that means we are dealing with shourt tag like <Mike> for actor
-      // or <#1 word1, word2 - translation of word; #2 word3, word4 - translation of word; />
-      tag = xmlString.match(/#/g) ? 'dict' : 'actor'
-    }
-
-    text = text.join(' ').replace(/<*(.+?)\/*\s*>/, '$1') // < >, < />, < / >
-    let tagObject = {}
-    if (tag === 'dict') {
-      tagObject = splitDictString(text)
-      return { [tag]: [...tagObject] }
-    } else if (tag === 'actor') {
-      tagObject = { name: text }
-    } else {
-      tagObject = { text }
-    }
-    return { [tag]: { ...tagObject } }
-  }
+function tagToObject(tag, phraseLine) {
+    return fullTagToObject(tag) ?
+        fullTagToObject(tag) :
+        shortTagToObject(tag, phraseLine);
 }
 
-/**
- * splits <DICT string /> into separate words,
- * and extracts for each word meaningful pieces:
- * each word substring contains
- * 1) number (order in the phrase) - optional
- * 2) word different forms (like singular and plural)
- * 3) translations
- */
-function splitDictString(dictString) {
-  const dictStringArray = dictString.split('#')
-  dictStringArray.shift()
-  const dictArray = dictStringArray.map(wordString => {
-    const wordStringArray = wordString.split(/\s+/)
-    const hasOrder = !!wordStringArray[0].match(/^\s*\d+/)
-    wordString = hasOrder ? wordStringArray.slice(1).join(' ') : wordString
-    const wordOrder = hasOrder ? +wordStringArray[0] : null
-    const [wordForms, wordTranslations] = wordString.split(' - ')
-    return { wordOrder, wordForms, wordTranslations }
-  })
-  return dictArray
-  // console.log(dictStringArray);
+function getDefaultAttribute(tag) {
+    const mapTagAttr = {
+        actor: "name", // <actor Neil / >
+        img: "src", // <img http://domain.com/file-path.jpg / >
+        comment: "text" // <comment text bla bla bla / >
+    };
+    return mapTagAttr[tag];
 }
 
-/* 
-const tests = [
-  `<ACTOR name="Neil Armostrong" src="url" />Hello, I'm Neil`,
-  `<Neil>Hello! Whats up?`,
-  `It is a good theme for discussion. <DICT #4 good - translation for good; #5 theme - translation of theme; #7 discussion - translation for discussion  />`
-]
+function getDefaultTag(xmlPositionInLine) {
+    const mapPositionTag = {
+        start: "actor", // <actor Neil / >
+        end: "comment", // <comment text bla bla bla / >
+        middle: ""
+    };
+    return mapPositionTag[xmlPositionInLine];
+}
 
-console.log(JSON.stringify(phraseTextToObject(tests[0]), null, ' '))
-console.log(JSON.stringify(phraseTextToObject(tests[1]), null, ' '))
-console.log(JSON.stringify(phraseTextToObject(tests[2]), null, ' '))
- */
+// start , middle , end
+function getPositionOfSubstringInString(substring, string) {
+    const startPattern = new RegExp("^\\s*" + substring);
+    const endPattern = new RegExp(substring + "\\s*$");
+    if (string.match(startPattern)) return "start";
+    if (string.match(endPattern)) return "end";
+    return "middle";
+}
