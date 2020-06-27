d83dae1f940ea44e1883ecb8916832a1ed2ed609
aparus <rustamapaev@gmail.com>
2020-06-27 04:42:23 +0300

    auto upload materials from webvtt files

---
+++ src/utils/aeneas/uploadVTT.js
@@ -0,0 +1,29 @@
+import { promises as fss } from 'fs'
+import fs from 'fs'
+import { parseSrtWebvtt } from '../phrases.js'
+import admin from '../firebaseAdmin/admin.js'
+
+const db = admin.firestore()
+
+const materialsDir = '../../../../aeneas-content/alices-adventures'
+
+fs.readdir(`${materialsDir}/import`, (err, files) => {
+    files.forEach((fileName, index) => {
+        // if (index !== 0) return
+        const uploadFile = async() => {
+            const readPath = `${materialsDir}/import/${fileName}`
+            const file = await fss.readFile(readPath, "utf8")
+            const { material } = parseSrtWebvtt(file)
+            const { materialId } = material
+            delete material.WEBVTT
+            delete material.materialId
+                // console.log(JSON.stringify(material, null, '\t'))
+            db.collection('material')
+                .doc(materialId)
+                .set(material)
+                .catch(error => console.log('error', error))
+        }
+        uploadFile()
+    })
+
+})
