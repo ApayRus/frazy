e0ae639d766c10c359fce6f1d995832b1a25fa2b
aparus <rustamapaev@gmail.com>
2020-07-04 02:29:58 +0300

    added cli param "lang, path"

--- src/utils/aeneas/rewriteVTT.js
+++ src/utils/aeneas/rewriteVTT.js
@@ -2,44 +2,42 @@
  * rewrite WEBVTT header with additional info, like:
  * mediaLink, unit, order, youtubeId, lang, title
  * for automatically upload on the next step
+ *
  */
-
 
 import { promises as fss } from 'fs'
 import fs from 'fs'
-const materialsDir = '../../../../aeneas-content/alices-adventures'
+// * $ node --experimental-json-modules rewriteVTT.js lz lz-sthayrin-yifen-gurush
+const [lang, bookFolder] = process.argv.slice(2)
+const materialsDir = `../../../../aeneas-content/${bookFolder}`
 fs.readdir(`${materialsDir}/output`, (err, files) => {
     files.forEach((fileName, index) => {
-        const newFileHeader = getFileHeader(fileName)
+        const newFileHeader = getFileHeader(fileName, bookFolder, lang)
 
         const rewriteFile = async() => {
             const readPath = `${materialsDir}/output/${fileName}`
             const writePath = `${materialsDir}/import/${fileName}`
-            const oldFile = await fss.readFile(readPath, "utf8")
+            const oldFile = await fss.readFile(readPath, 'utf8')
             const newFile = oldFile.replace('WEBVTT', newFileHeader)
             await fss.writeFile(writePath, newFile)
                 // console.log(newFile)
         }
         rewriteFile()
     })
-
 })
-
 
 // https://archive.org/download/aliceinwonderland_02_02/aliceinwonderland_01_01.mp3
 function getMediaLink(mediaCollectionId, fileName) {
     return `https://archive.org/download/${mediaCollectionId}/${fileName}.mp3`
 }
 
-function getFileHeader(fileName) {
-    const filePrefix = 'aliceinwonderland_'
+function getFileHeader(fileName, unit, lang) {
+    const filePrefix = unit
     const fileNameWithoutExt = fileName.replace(/\..+$/, '')
     const order = fileNameWithoutExt.replace(filePrefix, '').replace(/_/g, '-')
-    const materialPrefix = 'alice-in-wonderland-'
-    const materialId = `${materialPrefix}${order}`
-    const mediaLink = getMediaLink('aliceinwonderland_02_02', fileNameWithoutExt)
-    const unit = 'Alices-Adventures-In-Wonderland-En'
-    const lang = 'en'
+    const materialPrefix = unit
+    const materialId = `${materialPrefix}-${order}`
+    const mediaLink = getMediaLink(unit, fileNameWithoutExt)
 
     const newFileHeader = `WEBVTT
 materialId: ${materialId}
--- src/utils/aeneas/uploadVTT.js
+++ src/utils/aeneas/uploadVTT.js
@@ -5,14 +5,16 @@ import admin from '../firebaseAdmin/admin.js'
 
 const db = admin.firestore()
 
-const materialsDir = '../../../../aeneas-content/alices-adventures'
+//node --experimental-json-modules uploadVTT.js ru-crime-and-punishment
+const [bookFolder] = process.argv.slice(2)
+const materialsDir = `../../../../aeneas-content/${bookFolder}`
 
 fs.readdir(`${materialsDir}/import`, (err, files) => {
     files.forEach((fileName, index) => {
         // if (index !== 0) return
         const uploadFile = async() => {
             const readPath = `${materialsDir}/import/${fileName}`
-            const file = await fss.readFile(readPath, "utf8")
+            const file = await fss.readFile(readPath, 'utf8')
             const { material } = parseSrtWebvtt(file)
             const { materialId } = material
             delete material.WEBVTT
@@ -21,9 +23,8 @@ fs.readdir(`${materialsDir}/import`, (err, files) => {
             db.collection('material')
                 .doc(materialId)
                 .set(material)
-                .catch(error => console.log('error', error))
+                .catch((error) => console.log('error', error))
         }
         uploadFile()
     })
-
 })
